e5e524c3d8f2fa457becef19ba5fa479bcf266cc
joejoe2 <39475537+joejoe2@users.noreply.github.com>
2022-03-08 21:56:42 +0800

    Update EmailServiceTest.java

--- src/test/java/com/joejoe2/demo/service/EmailServiceTest.java
+++ src/test/java/com/joejoe2/demo/service/EmailServiceTest.java
@@ -9,10 +9,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EmailServiceTest {
     @MockBean
     JavaMailSender emailSender;
@@ -27,6 +29,8 @@ class EmailServiceTest {
         message.setTo("to");
         message.setSubject("subject");
         message.setText("content");
+
+        Mockito.doNothing().when(emailSender).send(message);
 
         emailService.sendSimpleEmail("to", "subject", "content");
         Mockito.verify(emailSender).send(message);
