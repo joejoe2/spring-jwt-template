b2b79d0087171e76bb0c6f4fea17ff884bb2fa6f
joejoe2 <7013601@gmail.com>
2023-12-24 21:21:34 +0800

    clean up TestContext

--- src/test/java/com/joejoe2/demo/TestContext.java
+++ src/test/java/com/joejoe2/demo/TestContext.java
@@ -6,15 +6,10 @@ import org.testcontainers.containers.GenericContainer;
 
 public class TestContext implements BeforeAllCallback, ExtensionContext.Store.CloseableResource {
 
-  private static boolean started = false;
-
   static {
     GenericContainer redis = new GenericContainer("redis:6.2.7-alpine").withExposedPorts(6379);
     redis.getPortBindings().add("6370:6379");
     redis.start();
-
-    System.setProperty("spring.redis.host", redis.getContainerIpAddress());
-    System.setProperty("spring.redis.port", redis.getFirstMappedPort() + "");
 
     GenericContainer postgres =
         new GenericContainer("postgres:15.1")
@@ -23,17 +18,11 @@ public class TestContext implements BeforeAllCallback, ExtensionContext.Store.Cl
             .withEnv("POSTGRES_DB", "spring-test");
     postgres.getPortBindings().add("5430:5432");
     postgres.start();
-    System.setProperty(
-        "spring.datasource.url",
-        "jdbc:postgresql://localhost:" + postgres.getFirstMappedPort() + "/spring-test");
   }
 
   @Override
   public void beforeAll(ExtensionContext context) {
-    if (!started) {
-      started = true;
-      // Your "before all tests" startup logic goes here
-    }
+    // Your "before all tests" startup logic goes here
   }
 
   @Override
